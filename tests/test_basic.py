5ea8b00d9b429751c679a91c977d2062fe2ab92b
ClaesFredo <claes.fredo@qringtech.com>
2019-07-18 15:37:07 +0200

    A bad and large commit of documentation and tests

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -2,3 +2,12 @@
 """ Code unit-tests go here. """
 
 import image_ema_project
+import numpy as np
+
+def test_roi_xy():
+    """ Test the roi_xy function """
+    xy = np.arange(-4, 5)
+    result = [xy,xy]
+    roi_xy = image_ema_project.roi_xy([0,0],[8,8])
+    #np.all tests that we have a match for all data
+    assert np.all(np.equal(roi_xy, result))
